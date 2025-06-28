c412aff0640fb1e44e10ea0548331a8d838d2833
rajaumarmehmood <rajaumarmehmood2617@gmail.com>
2025-06-28 18:09:42 +0500

    Update auth.ts

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://aquanestbackend-production.up.railway.app/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://aquanestbackend-production.up.railway.app//api";
 
 function getAuthHeaders() {
   if (typeof window === 'undefined') return {};
